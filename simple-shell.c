acd2c4aca740533fcbfc19248f62e47d8179711c
GunikaSingh <gunika505@gmail.com>
2024-09-24 16:34:40 +0530

    no pipe done

--- simple-shell.c
+++ simple-shell.c
@@ -7,8 +7,14 @@
 #include <time.h>
 
 #define MAX 1000
-char** historyArray; //declaring global variable to access in different functions 
-int num=0;   
+char* historyArray[MAX];  
+int historyCount=0;
+double runtimeArray[MAX];
+struct timeval startTime;
+struct timeval endTime;
+time_t timeArray[MAX];
+time_t* currtime;
+int pidArray[MAX];
 
 char* readInput() {
     char* buffer=(char*)malloc(MAX*sizeof(char*));
@@ -34,8 +40,7 @@ int parse(char* cmd,char** arr,char* delim){
     arr[size]=NULL;
     return size;}
 
-void createProcess(char* command){
-    historyArray[num++] = strdup(command);
+void runningProcess(char* command){
     char** arr = (char**)malloc(MAX * sizeof(char*));
     if (arr == NULL) {
         printf("Memory allocation failed\n");
@@ -43,8 +48,33 @@ void createProcess(char* command){
     }
     int arrsize=parse(command,arr,"|");
     if (arrsize==1){
-        //no pipe
-    }
+        int check=fork();
+        if (check == 0){
+            char** cmdLst=(char**)malloc(MAX * sizeof(char*));
+            parse(arr[0], cmdLst, " ");
+            int status=execvp(cmdLst[0], cmdLst);
+            if (historyCount<MAX){
+                historyArray[historyCount]=command;
+            }
+            else{
+                printf("Command Limit Exceeded\n");
+                exit(1);
+            }
+            gettimeofday(&startTime, NULL);
+            currtime=time(NULL);
+            timeArray[historyCount]=strdup(ctime(currtime))
+
+        } 
+        else if (check<0) {
+            printf("Something bad happened.\n");
+            exit(1);
+        } 
+        else {
+            pidArray[historyCount]=wait(NULL);
+            gettimeofday(&endTime, NULL);
+            runtimeArray[historyCount]=(endTime.tv_sec-startTime.tv_sec)+(endTime.tv_usec-startTime.tv_usec)/1000000.0;
+            historyCount++;
+    }}
     else{
         int pipes[arg_size][2];
         if (pipe(pipes[i])==-1){
@@ -60,7 +90,7 @@ void createProcess(char* command){
     }
     
     
-}
+}}
 
 void mainloop(){
     int repeat=1;
